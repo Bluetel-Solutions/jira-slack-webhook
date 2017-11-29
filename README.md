2186cf4514fb1ae3d536a92c411cfd8090d5e376
Alex Wilson <a@ax.gy>
2017-11-29 03:20:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# JIRA to Slack Webhook
+
+A simple Lambda picoservice for forwarding misc. webhooks from JIRA (e.g. those from Service Desk automation workflows)
+to any given Slack/Discord incoming webhook endpoint.
+
+Caveats:
+
+- Webhooks cannot be used to communicate with multiple channels. This is unlikely to change.
+- Message formatting is currently limited.
+
+
+#### Requirements
+
+- Yarn
+- Serverless
+
+
+#### Installation
+
+1. git clone https://github.com/bluetel-solutions/jira-slack-webhook.git && cd jira-slack-webhook
+2. cp .config.yml.dist .config.yml
+3. yarn install
+4. `serverless deploy --aws-profile <aws-profile-here> --stage prod`
+
+
+#### Configuration
+
+All configuration lives within `.config.yml`.
+It may also be overridden by environemnt variable (for CI workflows, etc.)
+
+```
+webhookUrls: A comma separated list of URLs of Slack/Discord webhooks to post to
+webhookTitle: The name and message used by the webhook user.  e.g. "JIRA".
+```
---
+++ index.js
@@ -0,0 +1,53 @@
+const {IncomingWebhook} = require('@slack/client')
+const denodeify = require('denodeify')
+
+const Issue = require('./entity/issue').Issue
+const User = require('./entity/user').User
+
+
+const webhooks = process.env.WEBHOOK_URLS.split(',')
+const sendToSlack = (webhook, message) => {
+  const incomingSlackWebhook = new IncomingWebhook(webhook)
+  const sendMessage = denodeify(incomingSlackWebhook.send.bind(incomingSlackWebhook), (header, statusCode, body) => {
+    return [header, statusCode, body]
+  })
+
+  return sendMessage(message)
+}
+const sendToAllSlacks = message => Promise.all(webhooks.map(webhook => sendToSlack(webhook, message)))
+
+
+module.exports.webhook = (event, context, callback) => {
+
+  const source = (event.body && event.body.length > 0) ? JSON.parse(event.body) : event
+
+  if (source.issue === undefined) {
+    callback(null, { statusCode: 500, body: "invalid response" } )
+  }
+
+  const issue = new Issue(source.issue)
+  const user = new User(source.user)
+
+  sendToAllSlacks({
+    text: process.env.WEBHOOK_TITLE,
+    attachments: [{
+      color: "#00CF00",
+      author_name: user.displayName,
+      author_icon: user.miniThumbnail,
+      fields: [{
+        title: "ID",
+        value: issue.key
+      }, {
+        title: "Title",
+        value: issue.title
+      }, {
+        title: "Description",
+        value: issue.description
+      }]
+    }],
+    username: process.env.WEBHOOK_TITLE,
+    iconEmoji: ':robot_face:'
+  })
+  .then(res => callback(null, { statusCode: 200, body: 'ok' } ))
+  .catch(err => callback(null, { statusCode: 500, body: JSON.stringify(err) } ))
+}
